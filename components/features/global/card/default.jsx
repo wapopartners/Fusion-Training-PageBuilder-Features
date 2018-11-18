3dbe68b01795de886e5e85f03a67c2b70475fd8b
Melissa Steffan <msteffan2012@gmail.com>
2018-11-18 16:17:40 -0500

    update feature to work with custom fields

--- components/features/global/card/default.jsx
+++ components/features/global/card/default.jsx
@@ -8,11 +8,15 @@ class Card extends Component {
   render () {
     const content = this.props.globalContent
     // We can extract our custom field values here, and even set default values if desired...
-    // const { breakingAlertText, showAuthorInfo = false } = this.props.customFields
-    console.log(this.props);
+    const { breakingAlertText, showAuthorInfo = false } = this.props.customFields
+    console.log(content);
     return (
       <article className='col-sm-12'>
-        <h1>{content.headlines.basic}</h1>
+        {breakingAlertText &&
+          <span>{breakingAlertText}</span>}
+        <h1>{content && content.headlines && content.headlines.basic}</h1>
+        {showAuthorInfo &&
+          <span>{content.credits.by[0].name}</span>}
       </article>
     )
   }
@@ -22,6 +26,11 @@ Card.propTypes = {
   customFields: PropTypes.shape({
     breakingAlertText: PropTypes.string,
     showAuthorInfo: PropTypes.bool
+  }),
+  globalContent: PropTypes.shape({
+    headlines: PropTypes.shape({
+      basic: PropTypes.string
+    })
   })
 }
 
