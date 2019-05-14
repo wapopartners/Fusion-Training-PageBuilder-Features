b1ee4d033b52e41cbd904f6d5afca4508b9c6b80
Luke Mason <luke.mason@washpost.com>
2019-05-14 14:00:44 -0500

    update to use fetch

--- components/features/movies/movie-list/default.jsx
+++ components/features/movies/movie-list/default.jsx
@@ -14,27 +14,24 @@ class MovieList extends Component {
 
   fetch () {
     const { movieQuery = "" } = this.props.customFields
-
-    const { fetched } = this.getContent('movie-list', { movieQuery: movieQuery, page: this.state.page }, '{ totalResults Search { Title Year Poster } }')
-
-    fetched.then(response => {
-      this.setState({
-        movies: [...this.state.movies, ...response.Search],
-        page: this.state.page + 1
+      this.fetchContent({
+        movies: {
+          source: 'movie-list',
+          query: { movieQuery: movieQuery, page: this.state.page }
+        }
       })
-    })
-  }
+    }
 
   render () {
     const { movies } = this.state
     const { headerText = "" } = this.props.customFields
-    const { topic = "" } = this.props.customFields
+    const { movieQuery = "" } = this.props.customFields
 
     return (
       <Fragment>
-        <h2>{topic}</h2>
+        <h2>{movieQuery}</h2>
         <div>
-          {movies && movies.map((movie, idx) =>
+          {movies && movies.Search && movies.Search.map((movie, idx) =>
             <div key={`movie-${idx}`}>
               <h4>{movie.Title}</h4>
               <p><strong>Year:</strong> {movie.Year}</p>
@@ -55,5 +52,7 @@ MovieList.propTypes = {
 
     })
   };
+  
+MovieList.static = true
 
 export default MovieList
